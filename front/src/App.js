5fc2b01e905cfa719e1bd6e92dae5e872d6cf779
kimcoyg <kjhkjh9753@naver.com>
2021-04-12 02:38:26 +0900

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from "./page/Home";
+
+const App = () => {
+  return (
+
+    <BrowserRouter>
+      <Route path="/" component={Home} exact/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ front/src/page/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <>
+            <div>kimjeongho</div>
+            <Button variant="primary">primary</Button>
+        </>
+    );
+}
+
+export default Home;
