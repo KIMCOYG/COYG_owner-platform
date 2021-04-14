be438e6bb2bb4d291c55913cc9324ebe3eea421c
DevSunbo <cowwhite1008@gmail.com>
2021-04-15 01:14:49 +0900

    로그인 된 상태에서 페이지 이동 등의 작업을 하기 위해서 만들어 놓은 임시 헤더

---
+++ front/src/component/HeaderOwner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Container, Row, Col, Image, Button} from 'react-bootstrap';
+import logo from '../static/image/home.png';
+
+const Header = () => {
+    return (
+        <Container className="mt-3">
+            <Row>
+                <Col>
+                    <Image src={logo}></Image>
+                </Col>
+                <Col>
+                    <Row className="justify-content-center">
+                        <Button className="col-xs-5" variant="link" value="마이페이지">마이페이지</Button>
+                        <div className="pt-1 col-xs-2"> / </div>
+                        <Button className="col-xs-5" variant="link" value="로그아웃">로그아웃</Button>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Header;
