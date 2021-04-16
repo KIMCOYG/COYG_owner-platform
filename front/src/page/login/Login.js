2ae7ebe4b60fef304caa324216da43803a57b516
kimcoyg <kjhkjh9753@naver.com>
2021-04-16 22:43:46 +0900

    로그인 페이지 일부 완성

--- front/src/page/login/Login.js
+++ front/src/page/login/Login.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import {Container, Row, Form} from 'react-bootstrap';
+import {Container, Form, Button} from 'react-bootstrap';
+import '../../App.css';
 
 const Login = () => {
     return (
         <Container>
-            <Row className="text-center">
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-                    <label className="form-check-label" htmlFor="inlineRadio1">customer</label>
-                </div>
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-                    <label className="form-check-label" htmlFor="inlineRadio2">ownser</label>
-                </div>
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-                    <label className="form-check-label" htmlFor="inlineRadio2">admin</label>
-                </div>
-            </Row>
+                    <div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                            <label className="form-check-label" htmlFor="inlineRadio1">customer</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                            <label className="form-check-label" htmlFor="inlineRadio2">ownser</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                            <label className="form-check-label" htmlFor="inlineRadio2">admin</label>
+                        </div>
+                    </div>
+                    <Form className="mt-3">
+                        <Form.Group contorlid="formEmail">
+                            <Form.Control type="email" placeholder="이메일을 입력해주세요"/>
+                        </Form.Group>
+                        <Form.Group contorlid="formPassword">
+                            <Form.Control type="password" placeholder="비밀번호를 입력해주세요"/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" size="sm" block>로그인</Button>
+                    </Form>
         </Container>
     );
 }
