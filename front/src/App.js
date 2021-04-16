0b5f6b297c1894030aa84d949a21e6b9aca3556a
kimcoyg <kjhkjh9753@naver.com>
2021-04-17 02:03:48 +0900

    OwnerJoin 완료

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from "./page/Home";
 import Login from './page/login/Login';
-import CustomerJoin from './page/login/CustomerJoin';
+// import CustomerJoin from './page/login/CustomerJoin';
+import OwnerJoin from './page/login/OwnerJoin';
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
     <BrowserRouter>
       <Route path="/" component={Home} exact/>
       <Route path="/login" component={Login} />
-      <Route path="/customer/join" component={CustomerJoin}></Route>
+      {/* <Route path="/customer/join" component={CustomerJoin}></Route> */}
+      <Route path="/customer/join" component={OwnerJoin}></Route>
     </BrowserRouter>
   );
 }
