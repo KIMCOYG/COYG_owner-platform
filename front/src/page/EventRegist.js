c76a5e77b2a12c12586b71a0075ec89f62b9c129
DevSunbo <cowwhite1008@gmail.com>
2021-04-17 20:18:59 +0900

    이벤트 등록 페이지 Form UI 와 등록 기능 구현예정

--- front/src/page/EventRegist.js
+++ front/src/page/EventRegist.js
@@ -1,24 +1,28 @@
 import React from 'react'
-import { Button, Container, Form } from 'react-bootstrap'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap'
 import Header from '../component/HeaderOwner'
 
 const RegistHeader = () => {
     return(
         <Row>
             <Button>◁</Button>
-            <Button>등록</Button>
-            {/* <Button onClick="document.getElementById('registEvent').submit();">등록</Button> */}
+            {/* <Button>등록</Button> */}
+            <Button onClick={document.getElementById('registEvent').submit()}>등록</Button>
         </Row>
     )
 }
 
+const handleSubmit = (event) => {
+    alert('A name was submitted: ');
+    event.preventDefault();
+}
 
 const EvnetRegist = () => {
     return(
         <Container>
             <Header></Header>
             <RegistHeader></RegistHeader>
-            <Form id="registEvent">
+            <Form onSubmit={handleSubmit} id="registEvent">
                 <Form.Group controlId="formShopName">
                     <Form.Label>가게명</Form.Label>
                     <Form.Control type="input" placeholder="가게명 입력"/>
@@ -29,22 +33,27 @@ const EvnetRegist = () => {
                     <Form.Control type="input" placeholder="이벤트명 입력"/>
                 </Form.Group>
 
-                <Form.Group controlId="formEventTerm">
-                    <Form.Label>이벤트기간</Form.Label>
-                    <Form.Control type="datepicker" />
-                    <Form.Control type="datepicker" />
-                </Form.Group>
+                <Form.Label>이벤트 기간</Form.Label>
+                <Form.Row className="align-items-center">
+                    <Form.Group as={Col} controlId="formEventStart">
+                        <Form.Label>시작일</Form.Label>
+                        <Form.Control  type="date" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formEventEnd">
+                        <Form.Label>종료일</Form.Label>
+                        <Form.Control  type="date" />
+                    </Form.Group>
+                </Form.Row>
 
                 <Form.Group controlId="formEventDetail">
                     <Form.Label>이벤트 내용</Form.Label>
-                    <Form.Control type="textarea"/>
+                    <Form.Control as="textarea" rows={4}/>
                 </Form.Group>
 
                 <Form.Group controlId="formEventDetail">
                     <Form.File id="formFile" label="사진첨부"/>
                 </Form.Group>
-
-                <Button variant="primary" type="submit"></Button>
+                <Button type="submit">등록</Button>
             </Form>
         </Container>
     )
