f708896e1d0ea6e80a8ffa5d7e75029fb9b3511d
hyj6125 <0312jj@naver.com>
2021-04-18 03:24:49 +0900

    사이드바 UI 추가 (클릭하면 컴포넌트 넘어가는거 미구현미구현)

--- front/src/page/AdminPage.js
+++ front/src/page/AdminPage.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from '../component/Header';
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 
 const AdminPage = () => {
-
     return (
         <>
-            <Header />
+        <Header/>
+        <ProSidebar>
+            <Menu iconShape="square">
+                <MenuItem>카테고리 관리</MenuItem>
+                <MenuItem>사장님 관리</MenuItem>
+            </Menu>
+        </ProSidebar>
         </>
     );
 }
+
+
 
 export default AdminPage;
