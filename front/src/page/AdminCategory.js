cc65260efcf3e0fe74206439da7d61370d800cc9
hyj6125 <0312jj@naver.com>
2021-04-18 19:54:05 +0900

    어드민 헤더 추가

--- front/src/page/AdminCategory.js
+++ front/src/page/AdminCategory.js
@@ -1,4 +1,4 @@
-import Header from '../component/Header';
+import Header from '../component/HeaderAdmin';
 import Sidebar from '../component/Sidebar';
 
 import React, { useState } from 'react';
--- front/src/page/AdminOwner.js
+++ front/src/page/AdminOwner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../component/Header';
+import Header from '../component/HeaderAdmin';
 import Sidebar from '../component/Sidebar'
 
 const AdminOwner = () => {
