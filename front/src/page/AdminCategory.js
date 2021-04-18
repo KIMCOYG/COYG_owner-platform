9965698457b0180ba93fb0066ce7d770ad496fb3
hyj6125 <0312jj@naver.com>
2021-04-18 17:21:47 +0900

    카테고리 grid sort, filter 기능 추가

--- front/src/page/AdminCategory.js
+++ front/src/page/AdminCategory.js
@@ -6,23 +6,19 @@ import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-
+//https://www.ag-grid.com/react-grid/getting-started/ 참고
 
 const data = [
     {id: 1, name: '생활용품', icon : 'sfsdf', createdAt : '2020/1/25', updatedAt : '2020/1/25', location : 1},
     {id: 2, name: '가전제품', icon : 'sadfsdf', createdAt : '2020/1/25', updatedAt : '2020/1/25', location : 2},
-    {id: 3, name: '도시락', icon : 'fewf', createdAt : '2020/1/25', updatedAt : '2020/1/25', location : 3},
+    {id: 3, name: '도시락', icon : 'fewf', createdAt : '2020/1/25', updatedAt : '2020/1/25', location : 3}
 ];
 
 const AdminCategory = () => {
     const [gridApi, setGridApi] = useState(null);
     const [gridColumnApi, setGridColumnApi] = useState(null);
 
-    const [rowData, setRowData] = useState([
-        { make: "Toyota", model: "Celica", price: 35000 },
-        { make: "Ford", model: "Mondeo", price: 32000 },
-        { make: "Porsche", model: "Boxter", price: 72000 }
-    ]);
+    const [rowData, setRowData] = useState(data);
 
     const onGridReady = params => {
         setGridApi(params.api);
@@ -33,13 +29,16 @@ const AdminCategory = () => {
         <>
         <Header/>
         <Sidebar/>
-        <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+        <div className="ag-theme-alpine" style={{ height: 400, width: 1200 }}>
             <AgGridReact
                 onGridReady={onGridReady}
                 rowData={rowData}>
-                <AgGridColumn field="make"></AgGridColumn>
-                <AgGridColumn field="model"></AgGridColumn>
-                <AgGridColumn field="price"></AgGridColumn>
+                <AgGridColumn field="id" sortable={ true } filter={ true }></AgGridColumn>
+                <AgGridColumn field="name" sortable={ true } filter={ true }></AgGridColumn>
+                <AgGridColumn field="icon" sortable={ true } filter={ true }></AgGridColumn>
+                <AgGridColumn field="createdAt" sortable={ true } filter={ true }></AgGridColumn>
+                <AgGridColumn field="updatedAt" sortable={ true } filter={ true }></AgGridColumn>
+                <AgGridColumn field="location" sortable={ true } filter={ true }></AgGridColumn>
             </AgGridReact>
         </div>
         </>
