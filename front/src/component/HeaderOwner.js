77ff198ecd68ee7196ecf3b9aba736bcb6fa52d0
DevSunbo <cowwhite1008@gmail.com>
2021-04-19 21:45:49 +0900

    link mypage

--- front/src/component/HeaderOwner.js
+++ front/src/component/HeaderOwner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Container, Row, Col, Image, Button} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import logo from '../static/image/home.png';
 
 const Header = () => {
@@ -11,9 +12,10 @@ const Header = () => {
                 </Col>
                 <Col>
                     <Row className="justify-content-center">
-                        <Button className="col-xs-5" variant="link" value="마이페이지">마이페이지</Button>
+                        <Button className="col-xs-5" variant="link" value="마이페이지">
+                            <Link to="/owner/mypage">마이페이지</Link>
+                        </Button>
                         <div className="pt-1 col-xs-2"> / </div>
-                        <Button className="col-xs-5" variant="link" value="로그아웃">로그아웃</Button>
                     </Row>
                 </Col>
             </Row>
