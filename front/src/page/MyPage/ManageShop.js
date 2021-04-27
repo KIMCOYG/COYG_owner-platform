7fc9122de3143dd21f2dea5bff235d7d9eda6464
DevSunbo <cowwhite1008@gmail.com>
2021-04-28 02:01:03 +0900

    todo 주석

--- front/src/page/MyPage/ManageShop.js
+++ front/src/page/MyPage/ManageShop.js
@@ -7,8 +7,10 @@ const SecondHeader = ({props}) => {
     return(
         <Row>
             <Button onClick={() => window.history.back()}>◁</Button>
-            {/* <Button>등록</Button> */}
+            
             <h5>가게 관리</h5>
+            {/* TODO: 2021.04.28 가게 추가 페이지 만들고 링크 연결 -sunbo */}
+            <Button>추가</Button>
         </Row>
     )
 }
@@ -38,10 +40,37 @@ const ManageShop = () => {
             <Header></Header>
             <SecondHeader></SecondHeader>
             <Form>
-                
+                <Form.Group>
+                    <Form.Label>가게이름</Form.Label>
+                    <Form.Control defaultValue="정호네꽃가게"></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>전화번호</Form.Label>
+                    <Form.Control defaultValue="정호네꽃가게"></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>사업자등록번호</Form.Label>
+                    <Form.Control value="정호네꽃가게"></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>영업시간</Form.Label>
+                    <Form.Control defaultValue="정호네꽃가게"></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>휴무일</Form.Label>
+                    <Form.Control defaultValue="정호네꽃가게"></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>주소</Form.Label>
+                    <Form.Control defaultValue="정호네꽃가게"></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>카테고리</Form.Label>
+                        <Categories ></Categories>
+                </Form.Group>
                 <Form.Group>
                     <Form.Label>상세정보</Form.Label>
-                        <Categories ></Categories>
+                    <Form.Control defaultValue="정호네꽃가게"></Form.Control>
                 </Form.Group>
                 
                 <Row>
