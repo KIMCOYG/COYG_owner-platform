e21f599ab45e37c225df432e46782ae4b0049185
hyj6125 <0312jj@naver.com>
2021-04-28 06:15:08 +0900

    카테고리 관리 추가 삭제 모달

--- front/src/page/admin/AdminCategory.js
+++ front/src/page/admin/AdminCategory.js
@@ -10,7 +10,9 @@ import BtnCellRenderer from "../../component/BtnCellRenderer.js";
 
 
 
-//https://www.ag-grid.com/react-grid/getting-started/ 참고
+//https://www.ag-grid.com/react-grid/getting-started/ 그리드 참고
+
+//https://getbootstrap.com/docs/4.0/components/modal/ 모달 참고
 
 const data = [
     {id: 1, name: '치킨', icon : 'chickenicon', createdAt : '2020/1/25', updatedAt : '2020/1/25', location : 1},
@@ -58,19 +60,38 @@ const AdminCategory = () => {
         
         <div style={{float:'left',marginTop: 30 , width:'33%'}}>
         <div className="ag-theme-alpine mx-auto" style={{ height: 400, width: 1110 }}>
-            <button style ={{marginBottom : 10, marginRight : 10, marginLeft : 1000}}type = "button" data-toggle="modal" data-target="#AddModal" className="btn btn-primary">추가</button>           
-            <button onClick={onButtonClick}>삭제</button>
+            <button style ={{marginRight : 10, marginLeft : 950}}type = "button" data-toggle="modal" data-target="#AddModal" className="btn btn-primary">추가</button>           
+            <button type = "button" data-toggle="modal" data-target="#DeleteModal" className="btn btn-primary">삭제</button>
             <div class="modal fade" id="AddModal" tabindex="-1" role="dialog" aria-labelledby="AddModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                    <h5 class="modal-title" id="AddModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="AddModalLabel">카테고리 추가</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                     </div>
                     <div class="modal-body">
-                    ...
+                    add
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="modal fade" id="DeleteModal" tabindex="-1" role="dialog" aria-labelledby="DeleteModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title" id="DeleteModalLabel">카테고리 삭제</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                    <div class="modal-body">
+                    delete
                     </div>
                     <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
