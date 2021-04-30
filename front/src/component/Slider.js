7bb35581a903d4dbaadf5c8e47c19813d8a20d65
kimcoyg <kjhkjh9753@naver.com>
2021-04-30 10:28:02 +0900

    login, postSearch 버튼 추가, slider 에러 해결

--- front/src/component/Slider.js
+++ front/src/component/Slider.js
@@ -2,24 +2,29 @@ import React from 'react';
 // import SimpleImageSlider from 'react-simple-image-slider';
 import { Carousel } from 'react-bootstrap';
 import chicken from '../static/image/chicken.jpg';
+import pizza from '../static/image/pizza.jpg';
+import laptop from '../static/image/laptop.jpg';
+import coffee from '../static/image/coffee.jpg';
 
 const Slider = () => {
   return (
     <>
-      <Carousel className="mt-3">
-        <Carousel.Item>
-          <img src={chicken} alt="chicken" className="d-block w-100" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={chicken} alt="chicken" className="d-block w-100" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={chicken} alt="chicken" className="d-block w-100" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={chicken} alt="chicken" className="d-block w-100" />
-        </Carousel.Item>
-      </Carousel>
+      <div className="w-100" style={{ minHeight: '250px' }}>
+        <Carousel className="mt-3" autoPlay={false} slide={false}>
+          <Carousel.Item>
+            <img src={chicken} alt="chicken" className="d-block w-100" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={pizza} alt="chicken" className="d-block w-100" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={laptop} alt="chicken" className="d-block w-100" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={coffee} alt="chicken" className="d-block w-100" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </>
   );
 };
