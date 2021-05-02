2804b9ae7490f7b35149cf4aa668d59d3ef9e364
kimcoyg <kjhkjh9753@naver.com>
2021-05-02 23:28:52 +0900

    mysql sequelize sync로 전환

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const mysql = require("mysql");
+const sequelize = require("./models/index").sequelize;
+// const mysql = require("mysql");
 // const bodyParser = require("body-parser");
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 
+// var connection = mysql.createConnection({
+//   host: "coyg.cwrvhsz1pk6u.ap-northeast-2.rds.amazonaws.com",
+//   port: 3306,
+//   user: "admin",
+//   password: "whwqkq3!",
+//   database: "coyg",
+// });
+// connection.connect();
+
 const PORT = 5000;
 
 const app = express();
+sequelize.sync().then(() => {
+  console.log("성공");
+});
 app.set("port", process.env.PORT || PORT);
 // app.get("/test", (req, res) => {
 //   connection.query("SELECT * FROM CATEGORIES", function (err, result, fields) {
