e6d8d3a08b7d6867859e66d6a4f21e244444e896
kimcoyg <kjhkjh9753@naver.com>
2021-05-15 20:11:54 +0900

    소프트웨어적 전화번호, 이메일 유니크

--- routers/authRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { isLoggedIn } from "../middlewares/authMiddleware";
-import routes from "../routes";
-
-const authRouter = express.Router();
-
-// Router.get(routes.profile, isLoggedIn, )
