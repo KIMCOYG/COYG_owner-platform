56eb588b667c1b50f135247a2e38e0263819229c
kimcoyg <kjhkjh9753@naver.com>
2021-05-15 21:25:45 +0900

    로그인 성공 / 미들웨어 및 세션 조작 필요

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import passport from "passport";
 import models from "../models";
 const User = models.User;
 
@@ -30,4 +31,45 @@ export const postJoin = async (req, res, next) => {
     console.log(error);
     return next(error);
   }
+};
+
+//LocalStrategy
+export const localLogin = async (email, password, done) => {
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      const result = await bcrypt.compare(password, exUser.password);
+      if (result) {
+        done(null, exUser);
+      } else {
+        done(null, false, { message: "비밀번호가 일치하지 않습니다." });
+      }
+    } else {
+      done(null, false, { message: "가입되지 않은 회원입니다." });
+    }
+  } catch (error) {
+    console.error(error);
+    done(error);
+  }
+};
+
+//globalRouter
+export const postLogin = async (req, res, next) => {
+  passport.authenticate("local", (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      req.flash("loginError", info.message);
+      return res.send(info.message);
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.send({ message: "로그인 성공" });
+    });
+  })(req, res, next);
 };
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,12 +2,13 @@ import express from "express";
 import routes from "../routes";
 import { hello } from "../controller/globalController";
 import { isLoggedIn, isNotLoggedIn } from "../middlewares/authMiddleware";
-import { getProfile, postJoin } from "../controller/authController";
+import { getProfile, postJoin, postLogin } from "../controller/authController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, hello);
 globalRouter.get(routes.profile, isLoggedIn, getProfile);
 globalRouter.post(routes.join, postJoin);
+globalRouter.post(routes.login, postLogin);
 
 export default globalRouter;
