7831f4de397944d9ef9898c9cc86deffe7b34944
kimcoyg <kjhkjh9753@naver.com>
2021-05-15 20:10:10 +0900

    회워가입 완료

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import models from "../models";
 const User = models.User;
 
@@ -7,8 +8,26 @@ export const getProfile = (req, res, next) => {
   });
 };
 
-export const postJoin = (req, res, next) => {
+export const postJoin = async (req, res, next) => {
   const { email, password, name, phone, role } = req.body;
-  const exUser = User.findOne({ where: { email } });
-  // .then()
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      req.flash("joinError", "이미 가입된 이메일입니다.");
+      return res.send({ message: "가입된 이메일" });
+    }
+    const hash = await bcrypt.hash(password, 12);
+    const result = await User.create({
+      email,
+      password: hash,
+      name,
+      phone,
+      role,
+      enabled: true,
+    });
+    return res.send({ result, message: hash });
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
 };
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
 import routes from "../routes";
 import { hello } from "../controller/globalController";
+import { isLoggedIn, isNotLoggedIn } from "../middlewares/authMiddleware";
+import { getProfile, postJoin } from "../controller/authController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, hello);
+globalRouter.get(routes.profile, isLoggedIn, getProfile);
+globalRouter.post(routes.join, postJoin);
 
 export default globalRouter;
