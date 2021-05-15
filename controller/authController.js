1d2e04789fd5cc01526c8f7ec59095c73b1a6c75
kimcoyg <kjhkjh9753@naver.com>
2021-05-15 20:13:03 +0900

    소프트웨어적 전화번호, 이메일 유니크 필요힘

--- controller/authController.js
+++ controller/authController.js
@@ -14,7 +14,7 @@ export const postJoin = async (req, res, next) => {
     const exUser = await User.findOne({ where: { email } });
     if (exUser) {
       req.flash("joinError", "이미 가입된 이메일입니다.");
-      return res.send({ message: "가입된 이메일" });
+      return res.send({ message: "가입된 이메일입니다." });
     }
     const hash = await bcrypt.hash(password, 12);
     const result = await User.create({
