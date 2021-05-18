eb0660dc0f91f3386abd57649489d194898065ca
YooJin Hong <0312jj@naver.com>
2021-05-18 23:39:57 +0900

    master pull 오류로인한 commit

--- app.js
+++ app.js
@@ -18,7 +18,25 @@ import likeRouter from "./routers/likeRouter";
 import scrapRouter from "./routers/scrapRouter";
 import eventRouter from "./routers/eventRouter";
 
+
+var MySQLStore = require("express-mysql-session")(session);
+
+var options = {
+  host: "coyg.cwrvhsz1pk6u.ap-northeast-2.rds.amazonaws.com",
+  port: 3306,
+  user: "admin",
+  password: "whwqkq3!",
+  database: "coyg",
+};
+
+var sessionStore = new MySQLStore(options);
+
 const PORT = 5000;
+const corsOptions = {
+  origin: "http://localhost:3000", // 접근 권한 부여 도메인
+  credentials: true, //응답 헤더에 access-control-allow credentials 추가
+  optionSuccessStatus: 200, //응답상태 200으로 설정
+};
 
 const app = express();
 
@@ -41,7 +59,9 @@ app.use(
   session({
     resave: false,
     saveUninitialized: false,
+    key: "coyg session",
     secret: "secret code",
+    store: sessionStore,
     cookie: {
       //store 고려!
       httpOnly: true,
