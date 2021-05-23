13f85780e49863e0694f125ac8a5cf42a6316cf3
kimcoyg <kjhkjh9753@naver.com>
2021-05-23 21:54:36 +0900

    SecondHeader 버튼 변경 중

--- front/src/component/SecondHeader.js
+++ front/src/component/SecondHeader.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import { Col, Row} from "react-bootstrap";
-import {BsChevronLeft} from "react-icons/bs";
-import {useHistory} from "react-router-dom";
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { BsChevronLeft } from 'react-icons/bs';
+import { useHistory } from 'react-router-dom';
 
-const SecondHeader = ({ title, buttonName, link, what}) => {
-    let history = useHistory();
+const SecondHeader = ({ title, buttonName, link }) => {
+  let history = useHistory();
 
-    return (
-        <Row>
-            <Col xs={1}>
-                <button onClick={() => history.goBack()} type="button" className="btn btn-link border-dark">
-                    <BsChevronLeft className="text-dark" />
-                </button>
-            </Col>
-            {/* <Button>등록</Button> */}
-            <Col xs={8}>
-                <h4 style={{marginLeft:'10px', fontSize: '20px'}}>{title}</h4>
-            </Col>
-            <Col xs={2}>
-                <button style={{fontSize: '6px'}} onClick={() => history.push(link)}>{buttonName}</button>
-            </Col>
+  return (
+    <Row>
+      <Col xs={1}>
+        <button
+          onClick={() => history.goBack()}
+          type="button"
+          className="btn btn-link border-dark"
+        >
+          <BsChevronLeft className="text-dark" />
+        </button>
+      </Col>
+      {/* <Button>등록</Button> */}
+      <Col xs={8}>
+        <h4 style={{ marginLeft: '10px', fontSize: '20px' }}>{title}</h4>
+      </Col>
+      <Col xs={2}>
+        <button
+          className="btn btn-primary"
+          style={{ fontSize: '6px' }}
+          onClick={() => history.push(link)}
+        >
+          {buttonName}
+        </button>
+      </Col>
 
-            {/* <Button onClick={() => this.handleSubmit()}></Button> */}
-        </Row>
-    )
-}
+      {/* <Button onClick={() => this.handleSubmit()}></Button> */}
+    </Row>
+  );
+};
 
-export default SecondHeader
+export default SecondHeader;
